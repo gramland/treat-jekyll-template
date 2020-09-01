792321caba2234bc432cfaf426463fb6c5bca6dc
Daniele Davì <1589956+gramland@users.noreply.github.com>
2020-09-01 20:44:59 +0200

    cookieconsent google analytics gemfile

---
+++ _includes/gtag.js
@@ -0,0 +1,10 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-0000000-00"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-0000000-00');
+</script>
+<!-- End Global site tag (gtag.js) - Google Analytics -->
